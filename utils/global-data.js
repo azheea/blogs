26f9a761c3fe1725fecbfa90832e7b4cbf9b1fc7
啊这 <87923452+azheea@users.noreply.github.com>
2022-12-04 14:42:00 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '啊这.的博客';
+    : '啊这.';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '啊这.的博客';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Love From AzheTeam.💕';
